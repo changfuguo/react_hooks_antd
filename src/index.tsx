7affe2db01a263387386b1075d7c06a1aa6b65f0
changfuguo <changfuguo@didichuxing.com>
2020-06-15 15:55:02 +0800

    初始化系统

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,10 @@ import ReactDOM from 'react-dom';
 import { ConfigProvider } from 'antd';
 import zh_CN from 'antd/es/locale-provider/zh_CN';
 import {Button} from 'antd'
-import '@/assets/styles/app.less';
-
+// import '@/assets/styles/app.less';
+import '@/index.less';
 ReactDOM.render((
     <ConfigProvider locale={zh_CN}>
-      <Button>常富国xx</Button>
+      <Button className="es-ant-btn" type="primary">常富国xx</Button>
     </ConfigProvider>
 ), document.getElementById('root'));
